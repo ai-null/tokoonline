6d52309bd1005c2cc05735c28289cdbfb0e36db1
GalangNouR <galangnour4@gmail.com>
2024-02-18 10:13:15 +0700

    update : PASSWORD ADA MERAH on LoginActivity & RegisterActivity

--- mobile/app/src/main/java/com/example/tokoonline/view/activity/LoginActivity.kt
+++ mobile/app/src/main/java/com/example/tokoonline/view/activity/LoginActivity.kt
@@ -1,22 +1,45 @@
 package com.example.tokoonline.view.activity
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.tokoonline.core.base.BaseAuthActivity
+import com.example.tokoonline.core.util.gone
+import com.example.tokoonline.core.util.visible
 import com.example.tokoonline.databinding.ActivityLoginBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+
+class PasswordViewModel : ViewModel() {
+    private val _passwordLength = MutableLiveData<Int>()
+    val passwordLength: LiveData<Int>
+        get() = _passwordLength
+
+    fun setPasswordLength(length: Int) {
+        _passwordLength.value = length
+    }
+
+}
+
 class LoginActivity : BaseAuthActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var viewModel: PasswordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this).get(PasswordViewModel::class.java)
 
         initListener()
+        observePasswordLength()
     }
 
     private fun initListener() = with(binding) {
@@ -37,8 +60,32 @@ class LoginActivity : BaseAuthActivity() {
             }
         }
 
+        edtPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.setPasswordLength(s?.length ?: 0)
+            }
+        })
+
         btnDaftarAkun.setOnClickListener{
             goToRegister()
         }
+    }
+
+    private fun observePasswordLength() {
+        viewModel.passwordLength.observe(this, { length ->
+            if (length < 8) {
+                binding.tvKurang.visible()
+            } else {
+                binding.tvKurang.gone()
+            }
+        })
     }
 }
--- mobile/app/src/main/java/com/example/tokoonline/view/activity/RegisterActivity.kt
+++ mobile/app/src/main/java/com/example/tokoonline/view/activity/RegisterActivity.kt
@@ -1,21 +1,35 @@
 package com.example.tokoonline.view.activity
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.tokoonline.core.base.BaseAuthActivity
+import com.example.tokoonline.core.util.gone
+import com.example.tokoonline.core.util.visible
 import com.example.tokoonline.databinding.ActivityRegisterBinding
 import com.example.tokoonline.data.model.firebase.User
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+
+
 class RegisterActivity : BaseAuthActivity() {
+    private lateinit var viewModel: PasswordViewModel
     private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this).get(PasswordViewModel::class.java)
 
          initListener()
+        observePasswordLength()
     }
 
     private fun initListener() = with(binding){
@@ -23,28 +37,45 @@ class RegisterActivity : BaseAuthActivity() {
             finish()
         }
 
-        btnRegister.setOnClickListener{
-            val email = edtEmail.text.toString()
-            val password = edtPassword.text.toString()
-            val nama = edtNama.text.toString()
-            val notelp = edtPhone.text.toString()
-            val role = "pembeli"
-            val userDomain = User(
-                email = email,
-                password = password,
-                nama = nama,
-                noTelepon = notelp,
-                role = role
-            )
+        edtPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
 
-            showProgressDialog()
-            register(userDomain = userDomain) {isSuccess ->
-                dismissProgressDialog()
-                if (isSuccess) onSuccess()
-                else showToast("Register gagal")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.setPasswordLength(s?.length ?: 0)
+            }
+        })
+
+
+
+        btnRegister.setOnClickListener{
+            if (binding.tvKurang.visibility == View.GONE) {
+                val email = edtEmail.text.toString()
+                val password = edtPassword.text.toString()
+                val nama = edtNama.text.toString()
+                val notelp = edtPhone.text.toString()
+                val role = "pembeli"
+                val userDomain = User(
+                    email = email,
+                    password = password,
+                    nama = nama,
+                    noTelepon = notelp,
+                    role = role
+                )
+
+                showProgressDialog()
+                register(userDomain = userDomain) {isSuccess ->
+                    dismissProgressDialog()
+                    if (isSuccess) onSuccess()
+                    else showToast("Register gagal")
+                }
             }
         }
-
         btnLoggiinnn.setOnClickListener{
             finish()
         }
@@ -55,5 +86,14 @@ class RegisterActivity : BaseAuthActivity() {
             delay(500)
             goToLoginActivity()
         }
+    }
+    private fun observePasswordLength() {
+        viewModel.passwordLength.observe(this, { length ->
+            if (length < 8) {
+                binding.tvKurang.visible()
+            } else {
+                binding.tvKurang.gone()
+            }
+        })
     }
 }
